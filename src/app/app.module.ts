bfdcdc1c6bb082e408c5ad0b295b32d335fae270
reda-cyber <redasmaidi2000@gmail.com>
2022-08-16 15:52:02 +0200

    first assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
