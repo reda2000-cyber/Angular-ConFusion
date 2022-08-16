e8cf27ecd7fca513c1d753b087ac4b79d151e445
reda-cyber <redasmaidi2000@gmail.com>
2022-08-16 21:10:41 +0200

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -12,6 +12,9 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import {DishService} from "./services/dish.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +31,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import {Comment} from './comment';
+
 export class Dish {
 
     id : string;
@@ -8,5 +10,6 @@ export class Dish {
     label : string;
     price : string;
     description : string;
+    comments : Comment[];
 
 }
