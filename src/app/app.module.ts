0fd359e2496fef2f572942ad7426d2f594dd73cf
reda-cyber <redasmaidi2000@gmail.com>
2022-08-17 19:44:55 +0200

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatDialogModule} from "@angular/material/dialog";
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -23,6 +23,7 @@ import {DishService} from "./services/dish.service";
 import {PromotionService} from "./services/promotion.service";
 import {LeaderService} from "./services/leader.service";
 import {AppRoutingModule} from "./app-routing/app-routing.module";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -44,12 +46,16 @@ import {AppRoutingModule} from "./app-routing/app-routing.module";
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
