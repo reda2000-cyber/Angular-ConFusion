02ae44c6d3cb60fe41a16113d0e2e7dc8cf3e30d
reda-cyber <redasmaidi2000@gmail.com>
2022-08-19 12:53:00 +0200

    RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import {Dish} from "../shared/dish";
 import {Params,ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
 import {DishService} from "../services/dish.service";
+import {switchMap} from "rxjs/operators";
 
 
 @Component({
@@ -14,18 +15,28 @@ export class DishdetailComponent implements OnInit {
 
 
   dish :Dish;
+  dishIds : string[];
+  prev : string;
+  next : string ;
 
   constructor(private dishservice :DishService,
               private location : Location,
               private route :ActivatedRoute) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe(dishes => this.dish = dishes);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+
   }
 
   goBack():void{
     this.location.back();
+  }
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
 }
